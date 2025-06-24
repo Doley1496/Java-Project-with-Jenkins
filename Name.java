618848cabdba0d409560e7e087847d23d3121513
madhur1496 <madhurdoley1496@gmail.com>
2025-06-24 17:39:01 +0530

    2nd-commit

--- Name.java
+++ Name.java
@@ -5,7 +5,8 @@ public class Name{
     public static void main(String[] args){
 
         System.out.println("Hello Everyone My 1st Java Project");
-        System.out.println("Hello my 1st commit to github using github desktop");
+        System.out.println("Hello my intial commit to github using github desktop");
+        System.out.println("Hello my 2nd commit to github using github desktop");
 
     }
 }
