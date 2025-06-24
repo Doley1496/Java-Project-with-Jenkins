7e6afc5fb59bf9b25dc97203e0cf67a8f841a88f
madhur1496 <madhurdoley1496@gmail.com>
2025-06-24 17:32:27 +0530

    Initial commit

---
+++ Name.java
@@ -0,0 +1,11 @@
+
+
+public class Name{
+    
+    public static void main(String[] args){
+
+        System.out.println("Hello Everyone My 1st Java Project");
+        System.out.println("Hello my 1st commit to github using github desktop");
+
+    }
+}
